ca20bc186e95e0c5e5468b851f2dcef5f3954c18
Ayusht777 <talesarayush@gmail.com>
2022-11-13 22:52:48 +0530

    day-111

---
+++ DAY-111/DAY-111-02.cpp
@@ -0,0 +1,78 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+//Ouick Sort
+//Quick Sort 
+//https://www.codingninjas.com/codestudio/problems/quick-sort_983625?leftPanelTab=0
+
+int partition( int arr[], int s, int e) {
+
+    int pivot = arr[s];
+
+    int cnt = 0;
+    for(int i = s+1; i<=e; i++) {
+        if(arr[i] <=pivot) {
+            cnt++;
+        }
+    }
+
+    //place pivot at right position
+    int pivotIndex = s + cnt;
+    swap(arr[pivotIndex], arr[s]);
+
+    //left and right wala part smbhal lete h 
+    int i = s, j = e;
+
+    while(i < pivotIndex && j > pivotIndex) {
+
+        while(arr[i] <= pivot) 
+        {
+            i++;
+        }
+
+        while(arr[j] > pivot) {
+            j--;
+        }
+
+        if(i < pivotIndex && j > pivotIndex) {
+            swap(arr[i++], arr[j--]);
+        }
+
+    }
+
+    return pivotIndex;
+
+}
+
+void quickSort(int arr[], int s, int e) {
+
+    //base case
+    if(s >= e) 
+        return ;
+
+    //partitioon karenfe
+    int p = partition(arr, s, e);
+
+    //left part sort karo
+    quickSort(arr, s, p-1);
+
+    //right wala part sort karo
+    quickSort(arr, p+1, e);
+
+}
+
+int main() {
+
+    int arr[6] = {9,8,7,1,2,0};
+    int n = 6;
+
+    quickSort(arr, 0, n-1);
+
+    for(int i=0; i<n; i++) 
+    {
+        cout << arr[i] << " ";
+    } cout << endl;
+
+
+    return 0;
+}
